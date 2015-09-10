066e1f2b9be78d589e6464f1f7e88d72a1cf8877
Yosem Sweet <yosem.sweet@gmail.com>
2015-09-10 00:37:48 -0700

    Load data on a page by page basis Added a cache layer and basic loading code - need to convert to data frame still and add column names

--- R/kiss-report.R
+++ R/kiss-report.R
@@ -6,17 +6,31 @@
 #'
 #' @param url An HREF to the report in KM as described by
 #'   \link{http://support.kissmetrics.com/api-update.html}
-#' @param interval The time range you want the report run for as a \code{lubridate::interval}
+#' @param interval The time range you want the report run for as a
+#'   \code{lubridate::interval}
 #'   (optional - can specify start and end instead)
-#' @param start The start date and time for the report (optional - can specify an interval instead)
-#' @param end The end date and time for the report (optional - can specify an interval instead)
-#' @return If url is a valid string or url (from \code{httr::url}) and the start and end dates are specified
-#'   This returns a new KissReport object that can be passed to the read S3 generic
+#' @param start The start date and time for the report (optional - can specify
+#'   an interval instead)
+#' @param end The end date and time for the report (optional - can specify an
+#'   interval instead)
+#' @param columnNames The names of columns for any produced reports
+#' @return If url is a valid string or url (from \code{httr::url}) and the start
+#'   and end dates are specified this returns a new KissReport object that can
+#'   be passed to the read S3 generic
+#' @examples
+#'    reportUrl <- "https://query.kissmetrics.com/v2/products/6581c29e-ab13-1030-97f2-22000a91b1a1/reports/1c564450-3586-0133-85e2-22000a9a8afc/run"
+#'    report <- KissReport(reportUrl,
+#'                        interval = lubridate::interval(as.Date("2015-06-01"), as.Date("2015-06-02")),
+#'                        columnNames = c("KM_Email", "KM_FirstUserId", "FirstVisitDate",
+#'                                        "FirstSource", "FirstMedium",
+#'                                        "FirstCampaignName", "FirstCampaignContent", "FirstCampaignTerms",
+#'                                        "FirstReferrer"))
+#'    reportResults <- read.KissReport(report)
 
 #' @export
-KissReport <- function(url, start, end, interval) {
+KissReport <- function(url, start, end, interval, columnNames) {
   if (!is.character(url) && !httr::is.url(url)) stop("url must be a string or url class")
-  if(is.null(interval)) {
+  if (is.null(interval)) {
     if (!lubridate::is.Date(start)) stop("If not creating with an interval, start must be a date")
     if (!lubridate::is.Date(end)) stop("If not creating with an interval, end must be a date")
     interval <- interval(start, end)
@@ -24,29 +38,59 @@ KissReport <- function(url, start, end, interval) {
     if (!lubridate::is.interval(interval)) stop("interval must be a valid interval")
   }
 
+  cache <- new.env(parent = emptyenv())
+
   url <- httr::parse_url(url)
   structure(list(
     url = url,
-    interval = interval),
+    interval = interval,
+    cache = cache,
+    columnNames = columnNames),
     class = "KissReport")
 }
+
+# return whatever is stored by cache
+readCache <- function (report, key) {
+  key <- as.cacheKey(key)
+  report$cache[[key]]
+}
+
+# return old value
+writeCache <- function (report, key, value) {
+  key <- as.cacheKey(key)
+
+  old = report$cache[[key]]
+  report$cache[[key]] = value
+
+  invisible(old)
+}
+
 
 #' Read data from a KissReport
 #'
 #' This function reads all the data from a KissMetrics people report
-#' returning everything in a data frame. Please note this is a synchronous function
-#' that can take quite some time to complete.
+#' returning everything in a data frame. Please note this is a synchronous
+#' function that can take quite some time to complete.
 #'
 #' @return  A \code{data.frame} containing all the data in the report
 #' @export
 read.KissReport <- function(report) {
   # Make request
   headers <- c(authorizationHeader(), jsonHeader())
-  response <- httr::POST(report$url,
-                         body = lapply(as.list(report$interval), as.timestamp),
-                         encode = "json",
+  body <- lapply(as.list(report$interval), as.timestamp)
+  encoding <- "json"
+
+  requestKey <- c(report$url, headers, body, encoding)
+  response <- readCache(report, requestKey)
+  if (is.null(response)) {
+    response <- httr::POST(report$url,
+                         body = body,
+                         encode = encoding,
                          httr::add_headers(.headers = headers))
-  httr::stop_for_status(response)
+
+    httr::stop_for_status(response)
+    writeCache(report, requestKey, response)
+  }
 
   # Get report status link from the response
   links  <- jsonlite::fromJSON(httr::content(response, "text"))$links
@@ -59,17 +103,23 @@ read.KissReport <- function(report) {
     response$data$completed
   }
 
-  resultsLink <- ""
+  resultsLink <- NULL
   repeat {
     # Wait 15 seconds for report to complete
     Sys.sleep(15)
 
     print("Checking status of report")
-    currentStatus <- jsonlite::fromJSON(httr::content(
-        httr::GET(statusLink, httr::add_headers(headers)),
-        "text"))
+    requestKey <- c(statusLink, headers)
+    response <- readCache(report, requestKey)
+    if (is.null(response)) {
+      response <- httr::GET(statusLink, httr::add_headers(headers))
+    }
+    httr::stop_for_status(response)
+    currentStatus <- jsonlite::fromJSON(httr::content(response, "text"))
+
     tries <- tries + 1
     if (reportComplete(currentStatus)) {
+      writeCache(report, requestKey, response)
       resultsLink <- with(currentStatus, links[links$name == "Results", "href"])
       break
     }
@@ -78,11 +128,56 @@ read.KissReport <- function(report) {
     }
   }
 
-  # resultsLink should now
+  # resultsLink should now be ready
   print("Report ready, pulling results")
+  results <- as.data.frame(loadResults(report, resultsLink), stringsAsFactors = FALSE)
+  if (!is.null(report$columnNames)) {
+    names(results) <- report$columnNames
+  }
+
+  results
+}
+
+buildResultPageUrls <- function(urlTemplate, offsets) {
+  matches <- "\\{\\{OFFSET\\}\\}"
+  stringr::str_replace_all(urlTemplate, matches, offsets)
+}
+
+loadPage <- function (resultUrl, report) {
+  headers <- c(authorizationHeader(), jsonHeader())
+  requestKey = c(resultUrl, headers)
+  response <- readCache(report, requestKey)
+  if (is.null(response)) {
+    response <- httr::GET(resultUrl, httr::add_headers(headers))
+    writeCache(report, requestKey, response)
+  }
+  results <- jsonlite::fromJSON(httr::content(response, "text"))
+
+  results$data[,-1]
+}
+
+loadResults <- function (report, resultsUrl) {
+  headers <- c(authorizationHeader(), jsonHeader())
+  requestKey = c(resultsUrl, headers)
+
+  response <- readCache(report, requestKey)
+  if (is.null(response)) {
+    response <- httr::GET(resultsUrl, httr::add_headers(headers))
+    writeCache(report, requestKey, response)
+  }
+  results <- jsonlite::fromJSON(httr::content(response, "text"))
+
+  totalResults <- results$pagination$total
+  resultsPerPage <- results$pagination$limit
+
+  resultsUrlTemplate <- stringr::str_c(resultsUrl,
+                                       "?limit=", resultsPerPage, "\u0026offset={{OFFSET}}")
 
 
+  resultsUrls <- buildResultPageUrls(urlTemplate = resultsUrlTemplate,
+                                     offsets = seq(from=0,
+                                                   to=totalResults,
+                                                   by=resultsPerPage))
 
-
-
+  do.call(rbind, lapply(resultsUrls, loadPage, report=report))
 }
